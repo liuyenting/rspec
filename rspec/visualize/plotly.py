b18802085aef54f255c1fb4c4be015faaedbb305
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-11-27 10:22:48 +0800

    Create simple line plots

---
+++ rspec/visualize/plotly.py
@@ -0,0 +1,9 @@
+import plotly.offline as ply
+import plotly.graph_objs as go
+
+def create_graph_obj(spectrum, name='Untitled', mode='lines'):
+    return go.Scatter(
+        name=name,
+        mode=mode,
+        x=spectrum['wavelength'], y=spectrum['intensity']
+    )
